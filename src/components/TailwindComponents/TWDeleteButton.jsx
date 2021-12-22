467acf03983c90f56c75b69325bdab7ae9f05535
Oleksandr Barylo <sbarylo20023@gmail.com>
2021-12-22 23:45:52 +0200

    Adjust remove btn

--- src/components/TailwindComponents/TWDeleteButton.jsx
+++ src/components/TailwindComponents/TWDeleteButton.jsx
@@ -3,6 +3,6 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export const TWDeleteButton = (props) => {
     return (
-        <button class="h-10 px-5 m-2 text-red-100 transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-red-800" type="danger" onClick={props.onClick}><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></button>
+        <button class=" h-14 w-14 px-5 m-2 text-red-100 transition-colors duration-150 bg-red-400 rounded-lg focus:shadow-outline hover:bg-red-500" type="danger" onClick={props.onClick}><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></button>
     );
 }
